95ec7d744c82c41aae7401a319e8caacb3e59c7e
Kevin Li <kevinli4321@gmail.com>
2019-12-04 11:22:22 -0500

    Ability to exit the shell

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  while (1) {
-    
+  char input[300] = ""; //support commands up to 300 characters long
+
+  while (strcmp(input, "exit") != 0) {
+    printf("$ ");
+    fgets(input, 300, stdin);
+    input[strlen(input) - 1] = '\0'; //strip the newline at the end
   }
+
+  printf("You have exited this shell. \n");
+  return 0;
 }
